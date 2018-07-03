644b4d4e9da6483a265fd484a8736ed8d3f7ce0b
Jiří Fryč <jirifryc.cz@jirifryc.cz>
2018-07-03 08:28:51 +0200

    :ok_hand: Code review.

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -1,17 +1,34 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
- * Created by CVUT CZM.
- * Author: Jiri Fryc
- * License: GNU GPLv3
+ * Privacy provider
+ *
+ * @package filter_linklight\privacy
+ * @category privacy
+ * @copyright 2018 CVUT CZM, Jiri Fryc
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 namespace filter_linklight\privacy;
 
+defined('MOODLE_INTERNAL') || die();
 
 use core_privacy\local\metadata\null_provider;
 
-class provider implements null_provider
-{
+class provider implements null_provider {
 
     /**
      * Get the language string identifier with the component's language
@@ -19,8 +36,7 @@ class provider implements null_provider
      *
      * @return  string
      */
-    public static function get_reason(): string
-    {
+    public static function get_reason(): string {
         return 'privacy:metadata';
     }
 }
--- filter.php
+++ filter.php
@@ -1,13 +1,29 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: frycj
- * Date: 02/05/2018
- * Time: 16:23
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ * LinkLight Filter
+ *
+ * @package /
+ * @category core
+ * @copyright 2018 CVUT CZM, Jiri Fryc
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+defined('MOODLE_INTERNAL') || die();
 
-class filter_linklight extends moodle_text_filter
-{
+class filter_linklight extends moodle_text_filter {
 
     /**
      * Override this function to actually implement the filtering.
@@ -16,34 +32,37 @@ class filter_linklight extends moodle_text_filter
      * @param array $options options passed to the filters
      * @return the HTML content after the filtering has been applied.
      */
-    public function filter($text, array $options = array())
-    {
+    public function filter($text, array $options = array()) {
         global $CFG;
-        $matches=[];
-        preg_match_all('/<a[^\>]*>/',$text,$matches,PREG_OFFSET_CAPTURE);
-        for($i=count($matches[0])-1;$i>=0;$i--) // We need to go from end. Otherwise we would break positioning
+        $matches = [];
+        preg_match_all('/<a[^\>]*>/', $text, $matches, PREG_OFFSET_CAPTURE);
+        for ($i = count($matches[0]) - 1; $i >= 0; $i--) // We need to go from end. Otherwise we would break positioning
         {
-            $match=$matches[0][$i];
-            if(count($match)==0)
+            $match = $matches[0][$i];
+            if (count($match) == 0) {
                 continue;
-            $pos=strpos($match[0],'href=')+5;
+            }
+            $pos = strpos($match[0], 'href=') + 5;
 
-            if(strpos($match[0],$CFG->wwwroot,$pos)==$pos+1)
-                $text=$this->internal_link($text,$match[1],strlen($match[0]));
-            else
-                $text=$this->external_link($text,$match[1],strlen($match[0]));
+            if (strpos($match[0], $CFG->wwwroot, $pos) == $pos + 1) {
+                $text = $this->internal_link($text, $match[1], strlen($match[0]));
+            } else {
+                $text = $this->external_link($text, $match[1], strlen($match[0]));
+            }
 
         }
         return $text;
     }
-    private function external_link(string $text,int $before_a,int $a_length)
-    {
-        $text=substr($text,0,$before_a)."&nbsp;".substr($text,$before_a,$a_length)."<i class='fa fa-globe'></i>".substr($text,$before_a+$a_length);
+
+    private function external_link(string $text, int $before_a, int $a_length) {
+        $text = substr($text, 0, $before_a) . "&nbsp;" . substr($text, $before_a, $a_length) . "<i class='fa fa-globe'></i>" .
+                substr($text, $before_a + $a_length);
         return $text;
     }
-    private function internal_link(string $text,int $before_a,int $a_length)
-    {
-        $text=substr($text,0,$before_a)."&nbsp;".substr($text,$before_a,$a_length)."<i class='fa fa-link'></i>&nbsp;".substr($text,$before_a+$a_length);
+
+    private function internal_link(string $text, int $before_a, int $a_length) {
+        $text = substr($text, 0, $before_a) . "&nbsp;" . substr($text, $before_a, $a_length) . "<i class='fa fa-link'></i>&nbsp;" .
+                substr($text, $before_a + $a_length);
         return $text;
     }
 }
